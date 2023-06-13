b9d1defb150bedbef2ffd7d6c965ccd8e849b2ef
vagrant <vagrant@machine1>
2023-06-13 23:23:37 +0000

    deleted old struct definition

--- mp1/emoji-translate.h
+++ mp1/emoji-translate.h
@@ -14,16 +14,9 @@ typedef struct _emoji_t {
     struct _emoji_indi *head;
 } emoji_t;
 
-
-
-
-
-
-
 void emoji_init(emoji_t *emoji);
 void emoji_add_translation(emoji_t *emoji, const unsigned char *source, const unsigned char *translation);
 char *find_translation(emoji_t * emoji, char *toInspect);
-//oid find_translation(emoji_t * emoji, char *toInspect, char* toFill);
 const unsigned char *emoji_translate_file_alloc(emoji_t *emoji, const char *fileName);
 void emoji_destroy(emoji_t *emoji);
 
